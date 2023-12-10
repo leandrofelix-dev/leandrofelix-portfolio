4cf7d91f87efd2829512a23fc1ff01c8a3b4a599
lehinfo-felix <lehinfo.felix@gmail.com>
2023-12-09 22:24:32 -0300

    feat: atualizando dados e imagens dos projetos

--- src/components/sections/index/optional/education.tsx
+++ src/components/sections/index/optional/education.tsx
@@ -1,41 +1,56 @@
-
 // Section structure
-import Section from '../../../structure/section';
-import Container from '../../../structure/container';
+import Section from "../../../structure/section";
+import Container from "../../../structure/container";
 
 // Section general blocks
-import SectionTitle from '../../../blocks/section.title'
+import SectionTitle from "../../../blocks/section.title";
 
 // Career scss
-import career from '../../../../styles/scss/sections/index/career.module.scss'
-
+import career from "../../../../styles/scss/sections/index/career.module.scss";
 
 export default function Education() {
-    return (
-        <Section classProp={`${career.section} borderBottom`}>
-            <Container spacing={['verticalXXXLrg']}>
-                <SectionTitle
-                    title="Education"
-                    preTitle="Formal"
-                    subTitle="I am currently studying computer science at the Velammal college of engineering and technology."
-                />
-                <section className={career.area}>
-
-                    <article className={career.company}>
-                        <div className={career.companyContent}>
-        <span className={career.companyHeader}>
-            <h3>Velammal College of Engineering and Technology</h3>
-            <h5>Madurai, Tamil Nadu</h5>
-        </span>
-                            <p>I am currently pursuing my Bachelors degree in Computer Science and Engineering at Velammal College of Engineering and Technology. This four-year program offers a comprehensive curriculum that covers various aspects of computer science and its applications.</p>
-
-                        </div>
-                        <div className={career.companyAlt}></div>
-                    </article>
-
-                </section>
-            </Container>
-        </Section>
-    )
+  return (
+    <Section classProp={`${career.section} borderBottom`}>
+      <Container spacing={["verticalXXXLrg"]}>
+        <SectionTitle
+          title="Education"
+          preTitle="Formal"
+          subTitle="I am currently studying information systems at the Federal Institute of Education, Science and Technology of Ceará."
+        />
+        <section className={career.area}>
+          <article className={career.company}>
+            <div className={career.companyContent}>
+              <span className={career.companyHeader}>
+                <h3>
+                  Federal Institute of Education, Science and Technology of
+                  Ceará
+                </h3>
+                <h5>Cedro, Ceará, Brazil</h5>
+              </span>
+              <p>
+                I am currently studying an 8-semester degree, in person at IFCE
+                Campus Cedro, in Information Systems, and I am currently in the
+                6th semester.
+              </p>
+            </div>
+            <div className={career.companyAlt}></div>
+          </article>
+          <article className={career.company}>
+            <div className={career.companyContent}>
+              <span className={career.companyHeader}>
+                <h3>
+                State school of professional education Dr José Iran Costa
+                </h3>
+                <h5>Várzea Alegre, Ceará, Brazil</h5>
+              </span>
+              <p>
+              I have a technical degree, in the IT area, through EEEP Dr. José Iran Costa, with a 3-year degree.
+              </p>
+            </div>
+            <div className={career.companyAlt}></div>
+          </article>
+        </section>
+      </Container>
+    </Section>
+  );
 }
-
